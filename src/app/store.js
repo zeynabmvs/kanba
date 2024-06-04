34e8aee1ef90102ec9263b86ff2d24df2864fa72
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-06-04 15:36:23 +0330

    reactivate redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -13,24 +13,24 @@ const rootReducer = combineReducers({
 });
 
 
-// const persistConfig = {
-//   key: 'kanbanAppState',
-//   storage,
-// };
-//
-// // Create a persisted reducer
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-//
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
-// export const persistor = persistStore(store);
+const persistConfig = {
+  key: 'kanbanAppState',
+  storage,
+};
 
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+export const persistor = persistStore(store);
+
+
+// export const store = configureStore({
+//   reducer: rootReducer
+// });
